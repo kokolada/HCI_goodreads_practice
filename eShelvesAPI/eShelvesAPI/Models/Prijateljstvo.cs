6f93c7e0c04fb0e270bc50f72699e1b6c46190f7
IB120133 <IB120133@d21d1406-d953-9443-9845-05699c70ec72>
2015-04-14 18:15:46 +0000

    model valjda gotovo

---
+++ eShelvesAPI/eShelvesAPI/Models/Prijateljstvo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace eShelvesAPI.Models
+{
+	public class Prijateljstvo
+	{
+		[Key, Column(Order = 0)]
+		public int Korisnik1ID { get; set; }
+		[Key, Column(Order = 1)]
+		public int Korisnik2ID { get; set; }
+		public int Status { get; set; }
+		public int UputioZahtjevID { get; set; }
+
+		public Korisnik Korisnik1 { get; set; }
+		public Korisnik Korisnik2 { get; set; }
+		//vidjet kako za Status koristik helper enumeraciju da ne stoji ruzni brojevi
+		//(tipa StatusType.Blocked, StatusType.Pending i tako)
+	}
+}
